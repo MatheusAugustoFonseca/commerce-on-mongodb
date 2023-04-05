0af4156ac1e028b005b72d80e29412e9fd4db408
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-04-05 16:55:28 -0300

    req 26 - Done

---
+++ challenges/desafio26.js
@@ -0,0 +1,20 @@
+db.produtos.updateMany(
+  { valoresNutricionais: {
+    $elemMatch: {
+      tipo: "sódio",
+      percentual: { $gt: 20, $lt: 40 },
+    },
+  },
+  },
+  { 
+    $push: {
+      tags: { 
+        $each: ["contém sódio"],
+        // $sort: { percentual: -1 }
+      },
+    },
+  },
+);
+
+ db.produtos.find({ }, {
+  _id: 0, nome: 1, tags: 1 });
